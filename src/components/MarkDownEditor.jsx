10b93bfa0a93e359eaa92425f47d76f38732a0d3
rani-4524 <166368361+rani-4524@users.noreply.github.com>
2026-02-08 18:16:23 +0530

    Update MarkDownEditor.jsx

--- src/components/MarkDownEditor.jsx
+++ src/components/MarkDownEditor.jsx
@@ -1,15 +1,31 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import ReactMarkDown from 'react-markdown'
 
 const MarkDownEditor = () => {
     const[markDown,setMarkDown] = useState("");
+    const[previewText ,setPreviewText]=useState("");
+    const[loading,setLoading] = useState(false);
 
+useEffect(()=>{
+  setLoading(true);
+  const timer = setTimeout(()=>{
+    setPreviewText(markDown);
+    setLoading(false);
+  },500);
+  return ()=> clearTimeout(timer);
+},[markDown]);
 
   return (
     <div>
       <textarea  className='textarea' value={markDown} onChange={(e)=>setMarkDown(e.target.value)} placeholder='write here'/>
         <div className='preview'>
-            <ReactMarkDown>{markDown}</ReactMarkDown>
+          {
+            loading ? (
+              <div className='loading'>Loading...</div>
+            ) : ( 
+              <ReactMarkDown>{previewText}</ReactMarkDown>
+            )
+          }
         </div>
     </div>
   )
